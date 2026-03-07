1b14cc737fe63bfc516f3e9d01c1dd92e9a112b3
ShadzXD <136199318+ShadzXD@users.noreply.github.com>
2026-03-07 10:29:24 +0100

    Update ClientPrefs.hx

--- source/backend/ClientPrefs.hx
+++ source/backend/ClientPrefs.hx
@@ -11,7 +11,7 @@ import Init;
 	public var downScroll:Bool = false;
 	public var middleScroll:Bool = false;
 	public var opponentStrums:Bool = true;
-	public var showFPS:Bool = true;
+	public var showFPS:Bool = false;
 	public var flashing:Bool = true;
 	public var autoPause:Bool = true;
 	public var antialiasing:Bool = true;
